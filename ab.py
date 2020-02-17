56fbb64108feb0537489146c925327451bf65cf9
samuel930514 <57798445+samuel930514@users.noreply.github.com>
2020-02-17 21:29:57 +0800

    Add files via upload

---
+++ ab.py
@@ -0,0 +1,26 @@
+import requests
+import random
+from flask import Flask
+import json
+
+app=Flask(__name__)
+
+@app.route("/Q")
+def Q():
+
+	Ans=""
+
+	Ansl = random.sample(range(0,10), 4)
+
+	for i in Ans1:
+		Ans+=str(i)
+
+	Ans2={"first":Ans[0],"second":Ans[1],"third":Ans[2],"fourth":Ans[3],"Ans":Ans}
+	Ansj = json.dumps(Ans2)
+
+	print (Ansj)
+	return "hello"
+
+if __name__=="__main__":
+	app.debug=True	
+	app.run()
